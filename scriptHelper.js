c6e44ea58ae4fab83f289e4f0e176f045534ccc1
Emran1233 <142645331+Emran1233@users.noreply.github.com>
2023-11-12 13:22:06 -0600

    Update scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,6 +1,6 @@
 // Write your helper functions here!
 
-require('cross-fetch/polyfill');
+// require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     // Here is the HTML formatting for our mission target div.
@@ -20,14 +20,14 @@ missionTarget.innerHTML = `<h2>Mission Destination</h2>
     `;
  }
  
- function validateInput(testInput) {
-    if (testInput === "") {
-    return "Empty";    
-}   else if (isNaN(testInput) === true){ 
-    return "Not a Number";
-}   else if (isNaN(testInput) === false){ 
+function validateInput(testInput)
+     if (testInput === ""){
+       return "Empty";    
+ }   else if (isNaN(testInput) === true){  
+       return "Not a Number";
+ }   else if (isNaN(testInput) === false){ 
         return "Is a Number";
-};
+ };
  
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
      let faultyStatus = document.getElementbyId("faultyStatus"); 
@@ -35,17 +35,16 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
      let copilotStatus = document.getElementById("copilotStatus"); 
      let fuelStatus = document.getElementById("fuelStatus"); 
      let cargoStatus = document.getElementById("cargoStatus"); 
-     let launchStatus = document.getElementById("launchStatus"); 
- }
-
+     let launchStatus = document.getElementById("launchStatus"); } 
+ 
 if   (validateInput(pilot.value) === "Is a Number"  
 ||    validateInput(copilot.value) === "Is a Number" 
 ||    validateInput(fuelLevel.value) === "Not a Number" 
 ||    validateInput(cargoLevel.value) === "Not a Number") { 
         alert("Invalid Input!");
     } else { 
-       pilotStatus.innerHTML = "Pilot: ${pilot.value}";
-       copilotStatus.innerHTML = "Co-pilot: ${copilot.value}";
+       pilotStatus.innerHTML = `Pilot: ${pilot.value}`;
+       copilotStatus.innerHTML = `Co-pilot: ${copilot.value}`;
     if  (fuelLevel.value < 10000) { 
          fuelStatus.innerHTML = "Not enough fuel for the journey"; 
          faultyItems.style.visibility = "visible"; 
@@ -61,8 +60,7 @@ if   (validateInput(pilot.value) === "Is a Number"
         launchStatus.style.color = "green"; 
     } 
 };
-    }
- 
+    
  async function myFetch() {
      let planetsReturned;
  
